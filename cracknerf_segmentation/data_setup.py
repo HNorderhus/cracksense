7f194744cfb6e702485fcf895479e605ccbd09c7
HNorderhus <henrik.norderhus@outlook.de>
2023-10-10 10:36:42 +0200

    changed iou to torchmetrics, added dilation to cracks

--- cracknerf_segmentation/data_setup.py
+++ cracknerf_segmentation/data_setup.py
@@ -17,44 +17,30 @@ class DataLoaderSegmentation(Dataset):
         self.mask_files = glob.glob(os.path.join(folder_path, 'Labels_grayscale', '*.*'))
         self.transform = transform
 
+    def dilate_cracks(self, mask):
+        # Define a kernel for dilation (you can adjust the size of the kernel as needed)
+        kernel = np.ones((5, 5), np.uint8)  # 5x5 kernel, adjust the size based on your requirements
+        dilated_mask = cv2.dilate(mask, kernel, iterations=1)
+        return dilated_mask
+
     def __getitem__(self, index):
         img_path = self.img_files[index]
         mask_path = self.mask_files[index]
 
         image = cv2.imread(img_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        #image = Image.open(img_path)
-        #mask = Image.open(mask_path)
         mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
 
+        # Check if the mask contains crack pixels (class 6)
+        if np.any(mask == 6):
+            # Perform dilation on the mask (cracks) only when there are crack pixels in the mask
+            mask = self.dilate_cracks(mask)
+
         if self.transform is not None:
-            transformed = self.transform(image=image, mask=mask)
+            transformed = self.transform(image=image, mask=mask)  # Pass the dilated mask to transform
             image = transformed["image"]
             mask = transformed["mask"]
 
-        #training / validation different transforms
-        # no augmentation for testing / validation
-
-      #  self.transforms = transforms.Compose([
-      #      transforms.RandomHorizontalFlip(), # ?
-      #      transforms.RandomVerticalFlip(), # ?
-      #      transforms.Resize(256, interpolation=transforms.InterpolationMode.NEAREST),
-            # transforms.RandomResizedCrop((512, 512)),
-            #transforms.RandomCrop((224, 224)),
-      #      transforms.ToTensor(),
-      #  ])
-
-        # Convert the image to a PyTorch tensor
-
-       # image = self.transforms(image)
-        #mask = self.transforms(mask)
-
-      #  normalize_transform = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-       # image = normalize_transform(image)
-
-        # mask = np.array(mask)
-        #mask = mask * 255
-        #mask = mask.long().squeeze()
         mask = mask.long()
 
         return image, mask
@@ -72,15 +58,15 @@ if debug_mode:
             A.LongestMaxSize(max_size=512, interpolation=1),
             A.RandomCrop(256, 256),
             A.PadIfNeeded(min_height=256, min_width=256),
-            A.VerticalFlip(p=0.2),
-            A.HorizontalFlip(p=0.2),
-            A.RandomRotate90(p=0.2),
+            A.VerticalFlip(p=0.3),
+            A.HorizontalFlip(p=0.3),
+            A.RandomRotate90(p=0.3),
             A.OneOf([
-                A.Blur(blur_limit=3, p=0.2),
-                A.ElasticTransform(alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03, p=0.1),
-                A.GridDistortion(p=0.2),
-                A.Transpose(p=0.2)
-            ], p=0.5),
+                A.Blur(blur_limit=3, p=0.3),
+                A.ElasticTransform(alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03, p=0.3),
+                A.GridDistortion(p=0.3),
+                A.Transpose(p=0.3)
+            ], p=0.3),
             A.RandomBrightnessContrast(p=0.2),
             A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             ToTensorV2(),
